8961ed3c61e01bf1d39e9b268390d69353799e91
sumanmanna134 <manna.suman134@gmail.com>
2024-09-26 19:22:46 +0530

    modify gitignore

---
+++ src/main/java/com/offlix/offlixstore/OfflixStoreApplication.java
@@ -0,0 +1,13 @@
+package com.offlix.offlixstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfflixStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfflixStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/offlix/offlixstore/OfflixStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.offlix.offlixstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfflixStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
